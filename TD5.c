8ef2c73551b5a79dcbd6567f28395d9d962197b0
lucasdebize <lucas.debize@edu.ece.fr>
2021-11-19 11:25:13 +0100

    Initial commit

---
+++ TD5.c
@@ -0,0 +1,9 @@
+#include "TD5.h"
+
+void tp5ex1(void) {
+    int n = sasirEntier();  //du tp 4
+    nombreRationnel nr;
+    nr.numerateur = 7;
+    nr.denominateur = 9;
+
+}
---
+++ TD5.h
@@ -0,0 +1,8 @@
+#ifndef TP5_TD5_H
+#define TP5_TD5_H
+
+typedef struct {
+    int numerateur, denominateur
+};
+void tp5ex1(void);
+#endif
